f3793130d00962d1b0ae91d6d58ae36d85009ac4
Manju <manjunath.k41@gmail.com>
2016-08-30 18:49:43 +0530

    attendance

--- public/attendance.js
+++ public/attendance.js
@@ -8,16 +8,25 @@ $(document).ready(function() {
         url:"https://my.linqs.in/conf/1/rooms",
         success : function (result) {
             console.log(result);
+            RoomListUpdate(result);
         },
         error : function (result) {
             console.log(result);
         }
     });
 
+    var RoomListUpdate = function(result){
+        var value = "";
+        for(var i=0;i<result.length;i++){
+            value = value + "<option id='"+result[i].roomId+"'>"+result[i].roomName+"</option>"
+        }
+    };
+
     $("#roomIdSelect").on('change', function () {
         console.log($(this).val());
         var roomId = $(this).attr('id');
-        $.ajax({
+        console.log(roomId);
+        /*$.ajax({
             type: "POST",
             data: {"confId": "1", "roomId": roomId},
             dataType: "json",
@@ -29,6 +38,6 @@ $(document).ready(function() {
             error: function (result) {
                 console.log(result);
             }
-        });
+        });*/
     });
 } );
