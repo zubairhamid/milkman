91f15325e939f2b65c2d24a59fac85e3b4188a06
zubairhamid <zubairhamid@cloudmpower.com>
2016-08-30 18:40:40 +0530

    server update

--- public/attendance.js
+++ public/attendance.js
@@ -2,13 +2,33 @@ $(document).ready(function() {
     $('#checkInData').DataTable();
 
     $.ajax({
-        url:"https://my.linqs.in/conf/1/rooms",
         type:"GET",
-        success: function (result) {
+        dataType: "json",
+        crossOrigin: true,
+        url:"https://my.linqs.in/conf/1/rooms",
+        success : function (result) {
             console.log(result);
         },
-        error : function () {
-
+        error : function (result) {
+            console.log(result);
         }
-    })
+    });
+
+    $("#roomIdSelect").on('change', function () {
+        console.log($(this).val());
+        var roomId = $(this).attr('id');
+        $.ajax({
+            type: "POST",
+            data: {"confId": "1", "roomId": roomId},
+            dataType: "json",
+            crossOrigin: true,
+            url: "https://my.linqs.in/conf/room/stats",
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (result) {
+                console.log(result);
+            }
+        });
+    });
 } );
